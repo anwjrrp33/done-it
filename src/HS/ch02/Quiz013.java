4c888e318cbbd5c73a1c248d5dd220797143e2c6
raccoonjeong <rana3782@gmail.com>
2022-06-07 23:04:01 +0900

    13번까지

---
+++ src/HS/ch02/Quiz013.java
@@ -0,0 +1,24 @@
+package HS.ch02;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+// 백준 2164 카드게임
+// 맞았다 ㅠㅠ
+public class Quiz013 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Deque<Integer> que = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) que.add(i);
+
+        while(que.size() != 1) {
+            que.poll();
+            que.add(que.poll());
+        }
+
+        System.out.println(que.peek());
+    }
+}
