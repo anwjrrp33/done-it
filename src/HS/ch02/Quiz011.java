a3f433a56f64afeee6a08d50249da0e4d3a40f8f
raccoonjeong <rana3782@gmail.com>
2022-06-07 13:22:57 +0900

    12번까지

--- src/HS/ch02/Quiz011.java
+++ src/HS/ch02/Quiz011.java
@@ -1,4 +1,125 @@
 package HS.ch02;
 
+import java.util.Scanner;
+import java.util.Stack;
+
+// 백준 1874 스택으로 오름차순 수열 만들기
+// 스택과 큐
 public class Quiz011 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
+        int num = 1;
+        boolean result = true;
+        for (int i = 0; i < N; i++) {
+            int su = A[i];
+            if (su >= num) {
+                while (su >= num) { // 딱 su == num이 될때까지 push후 pop
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            } else {
+                int n = stack.pop();
+                if (n > su) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                } else {
+                    // n == su라면 이게 맞지만
+                    // n < su인 경우는 어떻게 되는거지??
+                    bf.append("-\n");
+                }
+            }
+        }
+        if (result) System.out.println(bf.toString());
+    }
+
+    // 오답 (출력초과)
+//    public static void main(String[] args) throws IOException, EmptyStackException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//
+//        int count = Integer.parseInt(br.readLine());
+//
+//        int[] arr = new int[count];
+//
+//        for (int i = 0; i < count; i++) {
+//            arr[i] = Integer.valueOf(br.readLine());
+//        }
+//
+//        Stack stack = new Stack();
+//        int j = 1;
+//        stack.push(j);
+//        bw.write("+\n");
+//
+//        for (int i = 0; i < count; i++) {
+//            while(true) {
+//                if (!stack.isEmpty()
+//                        && (Integer) (stack.peek()) < arr[i]) {
+//                    j++;
+//                    stack.push(j);
+//                    bw.write("+\n");
+//                } else if (!stack.isEmpty()
+//                        && (Integer) (stack.peek()) == arr[i]) {
+//                    stack.pop();
+//                    bw.write("-\n");
+//                    break;
+//                } else if (!stack.isEmpty()
+//                        && (Integer) (stack.peek()) > arr[i]) {
+//                    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//                    bw.write("NO");
+//                    i = count;
+//                    break;
+//                } else if (stack.isEmpty()) {
+//                    j++;
+//                    stack.push(j);
+//                    bw.write("+\n");
+//                } else {
+//                    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//                    bw.write("NO");
+//                    i = count;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        bw.flush();
+//        bw.close();
+//        // 8
+//        // 4
+//        // 3
+//        // 6
+//        // 8
+//        // 7
+//        // 5
+//        // 2
+//        // 1
+//
+//        // 5
+//        // 1
+//        // 2
+//        // 5
+//        // 3
+//        // 4
+//
+//        // 5
+//        // 3
+//        // 2
+//        // 1
+//        // 4
+//        // 5
+//    }
+
 }
---
+++ src/HS/ch02/Quiz012.java
@@ -0,0 +1,74 @@
+package HS.ch02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+// 백준 17298 오큰수 구하기
+// 스택과 힙
+// 정답보고함
+// 이해안감
+public class Quiz012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] A = new int[n]; // 수열 배열 생성
+        int[] ANS = new int[n]; // 정답 배열 생성
+        String[] str = bf.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(str[i]);
+        }
+
+        // A배열의 인덱스가 쌓이는 스택임
+        Stack<Integer> myStack = new Stack<>();
+        myStack.push(0); // 비어있으므로 최초값 초기화
+
+        for (int i = 0; i < n; i++) {
+            // 스택이 비어있지 않고, 현재 수열이
+            // 스택 top 인덱스가 가리키는 수열보다 큰 경우
+            while(!myStack.isEmpty() && A[myStack.peek()] < A[i]) {
+                ANS[myStack.pop()] = A[i];
+            }
+            myStack.push(i);
+        }
+
+        while (!myStack.empty()) {
+            // 반복문을 다 돌고 나왔는데 스택이 비어있지 않다면 빌 때까지
+            ANS[myStack.pop()] = -1;
+            // 스택에 쌓인 index에 -1을 넣고
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < n; i++) {
+            bw.write(ANS[i] + " ");
+        }
+        bw.write("\n");
+        bw.flush();
+        
+    }
+
+//    public static void main(String[] args) throws Exception, EmptyStackException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int size = Integer.parseInt(br.readLine());
+//        // int[] arr = new int[size];
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(0);
+//        for (int i = 0; i < size; i++) {
+//            // arr[i] = Integer.parseInt(st.nextToken());
+//            int n = Integer.parseInt(st.nextToken());
+//            stack.push(n);
+//            if (stack.peek() < n) {
+//                System.out.println(stack.pop());
+//            } else {
+//                System.out.println("-1");
+//            }
+//        }
+//    }
+}
