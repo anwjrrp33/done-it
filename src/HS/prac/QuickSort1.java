50d24fcf3d872dd7045584f36036eb54c5296602
raccoonjeong <rana3782@gmail.com>
2022-06-16 13:30:59 +0900

    퀵정렬 공부

--- src/HS/prac/QuickSort1.java
+++ src/HS/prac/QuickSort1.java
@@ -1,11 +1,36 @@
 package HS.prac;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class QuickSort1 {
 
     public static void main(String[] args) {
 
     }
 
+    public static List<Integer> quickSort(List<Integer> list) {
+        if (list.size() <= 1) return list;
+        int pivot = list.get(list.size() / 2);
+
+        List<Integer> lesserArr = new LinkedList<>();
+        List<Integer> equalArr = new LinkedList<>();
+        List<Integer> greaterArr = new LinkedList<>();
+
+        for (int num : list) {
+            if (num < pivot) lesserArr.add(num);
+            else if (num > pivot) greaterArr.add(num);
+            else equalArr.add(num);
+        }
+
+        return Stream.of(quickSort(lesserArr), equalArr, quickSort(greaterArr))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+    }
 
 
 }
