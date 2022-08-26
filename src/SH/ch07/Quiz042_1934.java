3287a4774e69d2772accbea8b10cafc57fa495ca
anwjrrp33 <anwjrrp33@gmail.com>
2022-08-26 22:52:56 +0900

    Commit

---
+++ src/SH/ch07/Quiz042_1934.java
@@ -0,0 +1,25 @@
+package SH.ch07;
+
+import java.util.Scanner;
+
+public class Quiz042_1934 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = a * b / gcd(a, b);
+            System.out.println(result);
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b); // 재귀 함수의 형태로 구현
+        }
+    }
+}
